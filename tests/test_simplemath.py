73fdbbd368982680b2906eac08207457a48ae3d9
moemyself3 <moemyself3@gmail.com>
2019-06-20 13:00:39 -0500

    fixed test

--- tests/test_simplemath.py
+++ tests/test_simplemath.py
@@ -4,7 +4,7 @@ import simplemath as sm
 class TestSimpleMAth(unittest.TestCase):
     def test_add(self):
         ret = sm.add(1,3)
-        self.asserEqual(ret, 4)
+        self.assertEqual(ret, 4)
 
     def test_add_negint(self):
         ret = sm.add(-1,-3)
